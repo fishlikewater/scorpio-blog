3438ed9328b7181a3b6d88080df5cadb644a891d
fishlikewater <fishlikewater@126.com>
2017-12-04 16:49:25 +0800

    日程添加

--- src/main/java/scorpio/scorpioblog/agenda/dto/AgendaDTO.java
+++ src/main/java/scorpio/scorpioblog/agenda/dto/AgendaDTO.java
@@ -5,17 +5,29 @@ import scorpio.core.BaseObject;
 @Data
 public class AgendaDTO extends BaseObject {
 
-    private String id;
+    private static final long serialVersionUID = 1L;
+    private String id;//ID
+    private String title;//日程内容
+    private String startTime;//开始时间
+    private String endTime;//结束时间
+    private String allDay;//是否全天，1 - 是，0 - 不是
+    private String color;//颜色
+    private String userID;//用户ID
+    private String isFinish;//是否完成，1 - 是，0 - 不是
+    private String createTime;//创建时间
 
-    private String day;
-
-    private String beginTime;
-
-    private String endTime;
-
-    private String content;
-
-    private String updateTime;
-
-    private String scrq;
+    /**
+     * CREATE TABLE `Schedule` (
+     `id` varchar(32) NOT NULL COMMENT 'ID',
+     `title` varchar(100) DEFAULT NULL COMMENT '日程内容',
+     `startTime` datetime DEFAULT NULL COMMENT '开始时间',
+     `endTime` datetime DEFAULT NULL COMMENT '结束时间',
+     `allDay` varchar(2) DEFAULT NULL COMMENT '是否全天，1 - 是，0 - 不是',
+     `color` varchar(20) DEFAULT NULL COMMENT '颜色',
+     `userID` varchar(32) DEFAULT NULL COMMENT '用户ID',
+     `isFinish` varchar(2) DEFAULT '0' COMMENT '是否完成，1 - 是，0 - 不是',
+     `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+     PRIMARY KEY (`id`)
+     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+     */
 }
