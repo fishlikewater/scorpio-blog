ae5c925596e3f2d6bd04ad3f5485774549f662e9
fishlikewater <fishlikewater@126.com>
2017-12-05 17:19:51 +0800

    fullCalendar

--- src/main/resources/static/agenda/js/agenda.js
+++ src/main/resources/static/agenda/js/agenda.js
@@ -1,11 +1,11 @@
-var agenda = new function($) {
+var agenda = new function ($) {
 
     var layer;
     var form;
 
     return {
-        init: function() {
-            layui.use(['layer','form'], function () {
+        init: function () {
+            layui.use(['layer', 'form'], function () {
                 layer = layui.layer;
                 form = layui.form;
             })
@@ -28,16 +28,16 @@ var agenda = new function($) {
                 navLinks: true, // can click day/week names to navigate views
                 selectable: true,
                 selectHelper: true,
-                select: function(start, end) {
+                select: function (start, end) {
                     agenda.select(start, end);
                 },
                 /* dayClick: function(date){
 					console.log('dayClick触发的时间为：', date.format());
                 },*/
-                eventClick: function(event) {
+                eventClick: function (event) {
                     alert(event.title);
                 },
-                eventResize: function(event, dayDelta, revertFunc) {
+                eventResize: function (event, dayDelta, revertFunc) {
                     //do something here...
                     console.log(' --- start --- eventResize');
                     console.log('eventResize被执行，Event的title属性值为：', event.title);
@@ -53,7 +53,7 @@ var agenda = new function($) {
                     console.log('--- end --- eventResize');
                     // ...
                 },
-                eventDrop: function(event, dayDelta, revertFunc) {
+                eventDrop: function (event, dayDelta, revertFunc) {
                     //do something here...
                     console.log('eventDrop --- start ---');
                     console.log('eventDrop被执行，Event的title属性值为：', event.title);
@@ -75,25 +75,25 @@ var agenda = new function($) {
                 //日历初始化时显示的日期，月视图显示该月，周视图显示该周，日视图显示该天，和当前日期没有关系
                 //defaultDate: '2016-06-06',
                 //日程数据
-                events: function(start,end,timezone, callback) {
+                events: function (start, end, timezone, callback) {
                     var beginDay = start.format("YYYY-MM-DD")
                     var endDay = end.format("YYYY-MM-DD")
                     agenda.loadEvent(beginDay, endDay, callback)
                 },
-                
+
             });
 
-            $.each($.fullCalendar.locales, function(localeCode) {
+            $.each($.fullCalendar.locales, function (localeCode) {
                 $('#locale-selector').append(
                     $('<option/>')
-                    .attr('value', localeCode)
-                    .prop('selected', localeCode == initialLocaleCode)
-                    .text(localeCode)
+                        .attr('value', localeCode)
+                        .prop('selected', localeCode == initialLocaleCode)
+                        .text(localeCode)
                 );
             });
 
             // when the selected option changes, dynamically change the calendar option
-            $('#locale-selector').on('change', function() {
+            $('#locale-selector').on('change', function () {
                 if (this.value) {
                     $('#calendar').fullCalendar('option', 'locale', this.value);
                 }
@@ -102,11 +102,11 @@ var agenda = new function($) {
 
 
         //加载数据
-        loadEvent : function (beginDay, endDay, callback) {
+        loadEvent: function (beginDay, endDay, callback) {
             jQuery.get("/admin/agenda/list",
                 {
-                    beginDay:beginDay,
-                    endDay:endDay
+                    beginDay: beginDay,
+                    endDay: endDay
                 },
                 function (data) {
                     var events = [];
@@ -116,33 +116,43 @@ var agenda = new function($) {
         },
 
         //选择触发
-        select : function (start, end) {
-            layer.open({
-                title:'日程编辑'
-            })
+        select: function (start, end) {
             var dto = {
-                startTime:start.format(),
-                endTime:end.format()
+                startTime: start.format(),
+                endTime: end.format()
             }
-            jQuery.post("",
-                {
-
-                },function () {
-
-                })
-
-
-            var title = prompt('Event Title:');
-            var eventData;
-            if (title) {
-                eventData = {
-                    title: title,
-                    start: start,
-                    end: end
-                };
-                $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-            }
-            $('#calendar').fullCalendar('unselect');
+            layer.open({
+                title: '日程编辑',
+                type: 1,
+                skin: 'layui-layer-rim layui-layer-lan', //加上边框
+                area: ['420px', '240px'], //宽高
+                content: jQuery("#viewDiv"),
+                bnt: ['保存', '取消'],
+                'yes': function (index) {
+                    var content = jQuery("#content").text();
+                    if (content == "") {
+                        layer.msg("请输入内容", {icon: 2});
+                        return false;
+                    }
+                    jQuery.post("/admin/agenda/create", {dto: dto}, function (data) {
+                        if(data.code == 200){
+                            layer.close(index);
+                            eventData = {
+                                title: content,
+                                start: start,
+                                end: end
+                            };
+                            $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+                            $('#calendar').fullCalendar('unselect');
+                        }else{
+                            layer.msg("保存失败", {icon:2});
+                        }
+                    })
+                },
+                'bnt2': function (index) {
+                    layer.close(index);
+                }
+            });
         }
 
 
